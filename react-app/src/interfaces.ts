ad5f60f3decf32cd4589962b79b2ac086730efe4
Ken Chan <echan123@uottawa.ca>
2023-03-21 12:35:30 -0400

    404, Home and NavBar

---
+++ react-app/src/interfaces.ts
@@ -0,0 +1,24 @@
+export interface ISearchParams {
+  // Booking Info
+  // les dates (début, fin) de réservationou de location
+  reservationStartDate ?: Date,
+    reservationEndDate ?: Date,
+    bookingStartDate ?: Date,
+    bookingEndDate ?: Date,
+
+    // Room Info
+    //  la superficie
+    areaLower ?: number,
+    areaUpper ?: number,
+    //le prix des chambres
+    priceLower ?: number
+  priceUpper ?: number
+  capacity ?: number, // la capacité des chambres
+
+    // Hotel Info    
+    chain ?: string, // la chaîne hôtelière
+    category ?: number, // la catégorie (rating 1-5)
+    // le nombre total de chambres dans l’hôtel
+    numberOfRoomsLower ?: number,
+    numberOfRoomsUpper ?: number, 
+  }
--- react-app/src/pages/Home.tsx
+++ react-app/src/pages/Home.tsx
@@ -1,8 +1,23 @@
+import React from 'react';
+import { Card, Container, Image } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import backgroundHome from '../assets/backgroundHome.jpg';
+
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <Container fluid="xxl">
+      <Card className="position-relative">
+        <Image className="card-img-top opacity-25" src={backgroundHome} alt="BackgroundImage" fluid />
+        <Card.ImgOverlay className="card-img-overlay text-center">
+          <Card.Title className="display-5 text-center fw-bold p-1 d-inline">Hotel Mangement</Card.Title>
+          <Card.Footer className="bg-transparent border-0">
+            <div className="position-absolute bottom-0 mb-5 w-100 start-0">
+              <Link to="/search" className="btn btn-primary rounded-pill px-3 py-2 mx-3">Search for a Room</Link>
+            </div>
+          </Card.Footer>
+        </Card.ImgOverlay>
+      </Card>
+    </Container>
   );
 }
 
